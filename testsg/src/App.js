4e4d1d7041d5e4d6ec391a4d6c15dfecd259fb56
Abhijit <w2olves@hotmail.com>
2023-09-05 16:28:02 -0500

    Adding Cards

--- testsg/src/App.js
+++ testsg/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ProfileCard from "./ProfileCard";
+function App(){
+    return(<div>
+<div>Personal Digital Assistant</div>
+<ProfileCard/>
+<ProfileCard/>
+<ProfileCard/>
+</div>
+    )
 }
 
 export default App;
