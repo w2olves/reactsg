7728e00d8907c3557f5b452c2e7595dcaa6b89f5
Abhijit <w2olves@hotmail.com>
2023-09-05 17:28:21 -0500

    Adding Cards details

--- testsg/src/App.js
+++ testsg/src/App.js
@@ -2,9 +2,9 @@ import ProfileCard from "./ProfileCard";
 function App(){
     return(<div>
 <div>Personal Digital Assistant</div>
-<ProfileCard/>
-<ProfileCard/>
-<ProfileCard/>
+<ProfileCard title="Alexa" handle="@alexa"/>
+<ProfileCard title="Cortana" handle="@cort"/>
+<ProfileCard title="Siri" handle="@siri"/>
 </div>
     )
 }
