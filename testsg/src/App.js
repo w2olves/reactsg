00b4ba1123602e798a5c00b7dfa119f66cbee58e
Abhijit <w2olves@hotmail.com>
2023-09-11 14:58:42 -0400

    comm1

--- testsg/src/App.js
+++ testsg/src/App.js
@@ -1,13 +1,10 @@
-import ProfileCard from "./ProfileCard";
 function App() {
-  const alex =
-    "https://fastly.picsum.photos/id/200/200/200.jpg?hmac=mk1Tu6dXHQvpaA8RfxlDUZjbWG23krNkiB9kyYoEmO8";
+  const handleClick = () => {
+    console.log("clicked");
+  };
   return (
     <div>
-      <div>Personal Digital Assistant</div>
-      <ProfileCard title="Alexa" handle="@alexa" image={alex} />
-      <ProfileCard title="Cortana" handle="@cort" />
-      <ProfileCard title="Siri" handle="@siri" />
+      <button onClick={handleClick}>Show Animal</button>
     </div>
   );
 }
