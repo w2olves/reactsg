6dec4b57b6ac38cca5dc5185287556a82017c1ca
Abhijit <w2olves@hotmail.com>
2023-09-11 20:18:18 -0400

    comm2

--- testsg/src/App.js
+++ testsg/src/App.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
 function App() {
+  function getRandomAnimal() {
+    const animals = ["dog", "cat", "horse", "gator", "bird"];
+    return animals[Math.floor(Math.random() * animals.length)];
+  }
+  const [animals, setAnimals] = useState([]);
   const handleClick = () => {
-    console.log("clicked");
+    setAnimals([...animals, getRandomAnimal()]);
   };
+
+  const render = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
   return (
     <div>
       <button onClick={handleClick}>Show Animal</button>
+      {render}
     </div>
   );
 }
